52af0ace981f47ddaad789c02ba1b3f41eab9cf8
Fezzopro <fezzopro@gmail.com>
2023-10-13 12:18:58 +0200

    Add rails db migrations and rubocup configuration

---
+++ backend/db/migrate/20231013100653_create_replies.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateReplies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :replies do |t|
+      t.text :content
+      t.integer :likes
+      t.references :comment, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
