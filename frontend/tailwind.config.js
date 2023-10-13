23ef626469058e8fd557ceb6c2c7f6179aac1c38
Fezzopro <fezzopro@gmail.com>
2023-10-13 09:48:41 +0200

    Correct linters error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
@@ -11,5 +11,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
-
+};
