306b68b4e7cd795ead5a05b38b5c0be0bedfbd63
Fezzopro <fezzopro@gmail.com>
2023-10-13 12:18:08 +0200

    Add rails api backend starter

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -2,6 +2,8 @@
 
 require 'active_support/core_ext/integer/time'
 
+Rails.application.routes.default_url_options = { host: ENV.fetch('HOST'), port: ENV.fetch('PORT') }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -39,6 +41,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  # define default url options in environment files
+  config.action_mailer.default_url_options = { host: ENV.fetch('HOST'), port: ENV.fetch('PORT') }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
