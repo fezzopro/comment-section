106d4353d6d1af74d51005c01ce53f7b196d808b
Fezzopro <fezzopro@gmail.com>
2023-10-14 21:58:22 +0200

    Add aplication routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Comments from './components/Comments/Comments';
+import Signin from './components/Signin/Signin';
+import Signup from './components/Signup/Signup';
+import NoPage from './components/NoPage/NoPage';
+import Posts from './components/Posts/Posts';
 
 function App() {
   return (
-    <div className="container mx-auto px-4">
-      <div>
-        <Comments />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route index element={<Signin />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/comment" element={<Comments />} />
+        <Route path="/post" element={<Posts />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
