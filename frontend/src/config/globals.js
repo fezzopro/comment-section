54139af3681d867b809cc660532b05cfb748c1e6
Fezzopro <fezzopro@gmail.com>
2023-10-14 21:59:21 +0200

    Add configuration URLs

---
+++ frontend/src/config/globals.js
@@ -0,0 +1,7 @@
+export const HOST_URL = 'http://localhost:3001';
+export const API_URL = 'http://localhost:3001/api/v1';
+export const SIGNUP_URL = 'http://localhost:3001/api/v1/users';
+export const SIGNIN_URL = 'http://localhost:3001/users/tokens/sign_in';
+export const REDUX_DEVTOOLS = true;
+
+export default API_URL;
